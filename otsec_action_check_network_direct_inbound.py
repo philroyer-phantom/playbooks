2303af6200cb6924d798fb2727d248b164163483
admin <root@localhost>
2021-06-10 21:58:25 +0000

    OT Security Phantom Playbooks V1

--- otsec_action_check_network_direct_inbound.py
+++ otsec_action_check_network_direct_inbound.py
@@ -114,6 +114,7 @@ def Pin_Check_Network_New_Inbound(action=None, success=None, container=None, res
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_Network_Direct_Inbound')
 
     phantom.pin(container=container, data=formatted_data_1, message="Check Network Direct Inbound Sessions", name="Check Network Direct Inbound Sessions")
+    action_0(container=container)
 
     return
 
@@ -194,6 +195,17 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Network_Direct_Inbound, name="Search_OT_Asset")
+
+    return
+
+def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('action_0() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
