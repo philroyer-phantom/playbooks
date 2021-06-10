c38bab0ac7ae74bf3faf3bc1314273e05d8e33f9
admin <root@localhost>
2021-06-10 22:12:04 +0000

    Saved: OT Security Phantom Playbooks V1

--- otsec_action_check_software_not_allowed.py
+++ otsec_action_check_software_not_allowed.py
@@ -108,7 +108,6 @@ def Pin_Check_Software_Not_Allowed(action=None, success=None, container=None, re
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_Software_Not_Allowed')
 
     phantom.pin(container=container, data=formatted_data_1, message="Check for network behavior changes of asset", name="Check for network behavior changes of asset")
-    action_0(container=container)
 
     return
 
@@ -187,17 +186,6 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Software_Not_Allowed, name="Search_OT_Asset")
-
-    return
-
-def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('action_0() called')
-        
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    parameters = []
-
-    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
