7e9a855836f602bbf186c4ec28a41e6625ffbc3c
admin <root@localhost>
2021-06-10 21:51:33 +0000

    OT Security Phantom Playbooks V1

--- otsec_action_check_auth_via_network_session.py
+++ otsec_action_check_auth_via_network_session.py
@@ -110,6 +110,7 @@ def Pin_Check_Auth_Sessions_Network(action=None, success=None, container=None, r
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_Format_Auth_Sessions_Network')
 
     phantom.pin(container=container, data=formatted_data_1, message="Correlate network authentication session to assets with external hosts network activities", name="Correlate network authentication session to assets with external hosts network activities")
+    action_0(container=container)
 
     return
 
@@ -190,6 +191,17 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Auth_Sessions_Network, name="Search_OT_Asset")
+
+    return
+
+def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('action_0() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
