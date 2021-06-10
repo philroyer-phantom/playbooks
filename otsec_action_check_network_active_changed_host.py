f3cbed47094f62c42f015a4de7365590f244d924
admin <root@localhost>
2021-06-10 21:56:38 +0000

    OT Security Phantom Playbooks V1

--- otsec_action_check_network_active_changed_host.py
+++ otsec_action_check_network_active_changed_host.py
@@ -132,6 +132,7 @@ def Pin_Check_Network_New_Inbound(action=None, success=None, container=None, res
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_New_Active_Changed_Host')
 
     phantom.pin(container=container, data=formatted_data_1, message="Check Network New Active Changed Host", name="Check Network New Active Changed Host")
+    action_0(container=container)
 
     return
 
@@ -207,6 +208,17 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Network_New_Active_Changed_Host, name="Search_OT_Asset")
+
+    return
+
+def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('action_0() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
