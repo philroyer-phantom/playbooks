e4c3dbafb2efea83305811bb55ea5dab513a8868
admin <root@localhost>
2021-06-10 21:24:32 +0000

    OT Security Phantom Playbooks V1

--- otsec_action_check_access_acct_policy.py
+++ otsec_action_check_access_acct_policy.py
@@ -112,7 +112,6 @@ def Pin_Check_Access_Policy_Violations(action=None, success=None, container=None
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_Access_Policy_Violations')
 
     phantom.pin(container=container, data=formatted_data_1, message="Check Access Policy Violations", name="Check Network VPN Activity")
-    action_0(container=container)
 
     return
 
@@ -195,17 +194,6 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Access_Policy_Violations, name="Search_OT_Asset")
-
-    return
-
-def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug('action_0() called')
-        
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    parameters = []
-
-    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
