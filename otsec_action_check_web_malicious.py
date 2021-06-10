541dddb4189cdeba08d1738fe4ef0cf467e547c9
admin <root@localhost>
2021-06-10 22:11:25 +0000

    OT Security Phantom Playbooks V1

--- otsec_action_check_web_malicious.py
+++ otsec_action_check_web_malicious.py
@@ -108,6 +108,7 @@ def Pin_Check_Web_Malicious_Activity(action=None, success=None, container=None, 
     formatted_data_1 = phantom.get_format_data(name='Format_Pin_Web_Malicious_Activity')
 
     phantom.pin(container=container, data=formatted_data_1, message="Check Web Malicious Activity", name="Check Web Malicious Activity")
+    action_0(container=container)
 
     return
 
@@ -184,6 +185,17 @@ def Search_OT_Asset(action=None, success=None, container=None, results=None, han
     })
 
     phantom.act(action="run query", parameters=parameters, assets=['splunk es - ot sec'], callback=Format_Web_Malicious_Activity, name="Search_OT_Asset")
+
+    return
+
+def action_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug('action_0() called')
+        
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    phantom.act(action="<undefined>", parameters=parameters, name="action_0")
 
     return
 
